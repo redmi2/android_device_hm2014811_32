8e4db18d9360a737703571763cab75a23f788039
Gustavo Solaira <gustavos@codeaurora.org>
2014-08-22 14:22:25 -0300

    msm8916: add mixer control for skut1
    Add mixer control for skut1 device.
    
    Change-Id: Ibd879453d880cbc709af49b953b4fa970f8dcadb

--- msm8916_32.mk
+++ msm8916_32.mk
@@ -31,6 +31,7 @@ PRODUCT_COPY_FILES += \
     device/qcom/msm8916_32/mixer_paths_qrd_skuh.xml:system/etc/mixer_paths_qrd_skuh.xml \
     device/qcom/msm8916_32/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml \
     device/qcom/msm8916_32/mixer_paths_qrd_skuhf.xml:system/etc/mixer_paths_qrd_skuhf.xml \
+    device/qcom/msm8916_32/mixer_paths_qrd_skut1.xml:system/etc/mixer_paths_qrd_skut1.xml \
     device/qcom/msm8916_32/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
     device/qcom/msm8916_32/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml \
     device/qcom/msm8916_32/mixer_paths_wcd9330.xml:system/etc/mixer_paths_wcd9330.xml \
